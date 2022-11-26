1bf08686514c7cfc66acc1e7a1b5d659248a31ba
Matt <kanzoconfigz@hotmail.com>
2022-11-25 18:58:26 -0800

    Improved code, ground_name disabled for now

--- questgen.c
+++ questgen.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
@@ -33,7 +34,7 @@ static void append(char *text, int maxSize, char *toAppend) {
   snprintf(&(text[offset]), maxSize, "%s", toAppend);
 }
 
-static void GenQuest(MissionInfo mission)
+static void GenQuest(MissionInfo *mission)
 {
     FILE *fp;
     char buffer[512];
@@ -43,14 +44,17 @@ static void GenQuest(MissionInfo mission)
     char part5[64];
     char part6[64];
     char part7[64];
-    // put in the name
-    snprintf(buffer, 512, "SV.%s.Missions[\"%s\"] ", mission.ground, mission.name);
 
-    if (mission.type == 0)
+    // put in the name
+    // This is now hardcoded
+    //snprintf(buffer, 512, "SV.%s.Missions[\"%s\"] ", mission->ground, mission->name);
+    snprintf(buffer, 512, "SV.%s.Missions[\"%s\"] ", "missions", mission->name);
+
+    if (mission->type == 0)
     {
         snprintf(part2, 128, "= { Complete = COMMON.MISSION_INCOMPLETE, Type = COMMON.MISSION_TYPE_RESCUE, ");
     }
-    else if (mission.type == 1)
+    else if (mission->type == 1)
     {
         snprintf(part2, 128, "= { Complete = COMMON.MISSION_INCOMPLETE, Type = COMMON.MISSION_TYPE_ESCORT, ");
     }
@@ -61,25 +65,25 @@ static void GenQuest(MissionInfo mission)
 
     append(buffer, 128, part2);
 
-    snprintf(part3, 64, "DestZone = \"%s\", ", mission.zone);
+    snprintf(part3, 64, "DestZone = \"%s\", ", mission->zone);
     append(buffer, 64, part3);
-    snprintf(part4, 64, "DestSegment = %d, ", mission.segment);
+    snprintf(part4, 64, "DestSegment = %d, ", mission->segment);
     append(buffer, 64, part4);
-    snprintf(part5, 64, "DestFloor = %d, ", mission.floor);
+    snprintf(part5, 64, "DestFloor = %d, ", mission->floor);
     append(buffer, 64, part5);
 
 
-    if (mission.type == Escort)
+    if (mission->type == Escort)
     {
-        snprintf(part6, 64, "TargetSpecies = \"%s\", ", mission.client);
+        snprintf(part6, 64, "TargetSpecies = \"%s\", ", mission->client);
         append(buffer, 64, part6);
 
-        snprintf(part7, 64, "EscortSpecies = \"%s\" }", mission.escort);
+        snprintf(part7, 64, "EscortSpecies = \"%s\" }", mission->escort);
         append(buffer, 64, part7);
     }
     else
     {
-        snprintf(part6, 64, "TargetSpecies = \"%s\" }", mission.client);
+        snprintf(part6, 64, "TargetSpecies = \"%s\" }", mission->client);
         append(buffer, 64, part6);
     }
 
@@ -93,34 +97,35 @@ static void GenQuest(MissionInfo mission)
 int main()
 {
 
-    MissionInfo mission;
-    memset(&mission, 0, sizeof(MissionInfo));
+    MissionInfo *mission = malloc(sizeof(MissionInfo));
+    memset(mission, 0, sizeof(MissionInfo));
 
     printf("Welcome to Rev's PMDO Quest generator\n");
     printf("This tool will ask you questions about what your quest will be and will generate some quest data for you.\n");
 
-    printf("What is the ground map name?\n");
-    scanf("%s", mission.ground);
+    //printf("What is the ground map name?\n");
+    //scanf("%s", mission.ground);
     printf("What type of quest? (0 = Rescue, 1 = Escort, 2 = Outlaw)\n");
-    scanf("%d", &mission.type);
+    scanf("%d", &mission->type);
     printf("What is the quest name?\n");
-    scanf("%s", mission.name);
+    scanf("%s", mission->name);
     printf("What is the dungeon name for this quest? (zone_name)\n");
-    scanf("%s", mission.zone);
+    scanf("%s", mission->zone);
     printf("What is the dungeon segment for this quest? (default = 0)\n");
-    scanf("%d", &mission.segment);
+    scanf("%d", &mission->segment);
     printf("What floor does the quest take place at? (default = 1)\n");
-    scanf("%d", &mission.floor);
+    scanf("%d", &mission->floor);
     printf("Who is the target? (name of pokemon in lowercase)\n");
-    scanf("%s", mission.client);
+    scanf("%s", mission->client);
 
-    if (mission.type == Escort)
+    if (mission->type == Escort)
     {
         printf("Who is the escort? (name of pokemon in lowercase)\n");
-        scanf("%s", mission.escort);
+        scanf("%s", mission->escort);
     }
 
     GenQuest(mission);
+    free(mission);
 
     return 0;
 }
